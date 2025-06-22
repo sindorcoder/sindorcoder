d4822132800aa642afb73fbc4395270d4e78a6cf
Sindor Olmasov <162941627+sindorcoder@users.noreply.github.com>
2025-06-22 11:45:19 +0500

    Update Icons.tsx

--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -28,15 +28,6 @@ export const Icons = {
       />
     </svg>
   ),
-  x: () => (
-    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <title>X</title>
-      <path
-        fill="currentColor"
-        d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
-      />
-    </svg>
-  ),
   github: () => (
     <svg width="16px" viewBox="0 0 438.549 438.549">
       <path
@@ -64,11 +55,7 @@ export const DATA = {
         url: "www.linkedin.com/in/sindorolmasov",
         icon: Icons.linkedin,
       },
-      X: {
-        name: "X",
-        url: "https://x.com/sindorcoder",
-        icon: Icons.x,
-      },
+      
       email: {
         name: "Send Email",
         url: "mailto:sindordev77@gmail.com",
