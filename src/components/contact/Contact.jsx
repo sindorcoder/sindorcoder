24c4057b01656dc0d94d8e0891f2d614412b85ac
sindorcoder <sindordev77@gmail.com>
2025-09-16 17:03:03 +0500

    Update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,30 +1,35 @@
-import {social} from "../ui/Icons.tsx";
-import {Link} from "react-router-dom";
-import {BlurFade} from "../magicui/blur-fade.tsx";
-import {Fragment} from "react";
+import { social } from "../ui/Icons.tsx";
+import { Link } from "react-router-dom";
+import { BlurFade } from "../magicui/blur-fade.tsx";
+import { Fragment } from "react";
 
 const BLUR_FADE_DELAY = 0.04;
 const Contact = () => {
-
-    return (
-        <>
-                <div className={'flex flex-wrap w-full justify-center gap-2'}>
-
-                    {
-                        social.map((social, index) => (
-                            <Fragment key={index}>
-                                <BlurFade delay={BLUR_FADE_DELAY * 10 + index * 0.1} className={'flex  gap-2'}>
-                                    <Link to={social.url} target={'_blank'} rel="noopener noreferrer"
-                                          className={'!bg-[#fff] rounded-xl shadow border-[1px] border-[#f5f5f5] p-3'}>
-                                            {<social.icon/>}
-                                    </Link>
-                                </BlurFade>
-                            </Fragment>
-                        ))
-                    }
-                </div>
-        </>
-    );
+  return (
+    <>
+      <div className={"flex flex-wrap w-full justify-center gap-2"}>
+        {social.map((social, index) => (
+          <Fragment key={index}>
+            <BlurFade
+              delay={BLUR_FADE_DELAY * 10 + index * 0.1}
+              className={"flex  gap-2"}
+            >
+              <Link
+                to={social.url}
+                target={"_blank"}
+                rel="noopener noreferrer"
+                className={
+                  "!bg-[#fff] rounded-xl hover:scale-110 duration-500 shadow border-[1px] border-[#f5f5f5] p-3"
+                }
+              >
+                {<social.icon />}
+              </Link>
+            </BlurFade>
+          </Fragment>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Contact;
--- src/routes/skills/Skills.jsx
+++ src/routes/skills/Skills.jsx
@@ -1,40 +1,55 @@
-import { techsIcon } from "../../data";
 import { BlurFade } from "../../components/magicui/blur-fade";
-import { Badge } from "../../components/ui/badge";
-import github from "../../assets/icons/github.png"
-import {Link} from "react-router-dom";
+import github from "../../assets/icons/githubProfile.svg";
+import { Link } from "react-router-dom";
 const BLUR_FADE_DELAY = 0.04;
 const Skills = () => {
   return (
-    <section className="mt-[70px]">
-        <BlurFade delay={BLUR_FADE_DELAY * 10 + 2  * 0.05}>
-            <div className={'flex justify-center'}>
-                <div className={'border-4 p-4 max-w-[550px] bg-white rounded-2xl'}>
-                    <div className={'rounded-2xl border overflow-hidden border-gray-300'}>
-                        <img
-                            alt="github profile img"
-                            loading="lazy"
-                            className="w-full h-[320px] "
-                            src={github}
-                        />
-                    </div>
-                    <div className={'flex flex-col items-start gap-2 mt-5'}>
-                        <h5 className={'title text-[19px] font-normal'}>
-                            All Projects
-                        </h5>
-                        <span className={'text text-[17px] font-normal text-[#525252]'}>
-                    All my projects are here, you can access them if you want. I’ll keep updating this space regularly, so feel free to check back for new additions.
-                </span>
-                    </div>
-                    <Link to={'https://github.com/sindorcoder'} className={'mt-4 inline-block bg-black px-8 py-2 font-medium text-[15px] text-white rounded-3xl'}>
-                        View All
-                    </Link>
-                </div>
-
+    <section className="mt-[50px]">
+      <BlurFade delay={BLUR_FADE_DELAY * 10 + 2 * 0.05}>
+        <div className={"flex justify-center"}>
+          <div className={"border-4 p-4 max-w-[500px]  bg-white rounded-2xl"}>
+            <div
+              className={"rounded-2xl border overflow-hidden border-gray-300"}
+            >
+              <img
+                alt="github profile img"
+                loading="lazy"
+                className="w-full h-[240px] object-cover bg-white"
+                src={github}
+              />
             </div>
 
-        </BlurFade>
-            <div className="flex min-h-0 flex-col items-center my-10 gap-y-3">
+            <div className={"flex flex-col items-start gap-2 mt-5"}>
+              <h5 className={"title text-[14px] sm:text-[17px] font-normal"}>
+                All Projects
+              </h5>
+              <span
+                className={
+                  "text text-[10px] sm:text-[15px] font-normal text-[#525252]"
+                }
+              >
+                All my projects are here, you can access them if you want. I’ll
+                keep updating this space regularly, so feel free to check back
+                for new additions.
+              </span>
+            </div>
+            <Link
+              to={"https://github.com/sindorcoder"}
+              className={
+                "mt-4 inline-block bg-black title px-8 py-2 font-medium text-[10px]  sm:text-[12px] text-white rounded-3xl"
+              }
+            >
+              See Projects
+            </Link>
+          </div>
+        </div>
+      </BlurFade>
+      <div className="flex items-center justify-center mt-10">
+        <span className="text-[14px] font-normal text-[#525252] opacity-80">
+          © 2025 Sindor Coder | All rights reserved
+        </span>
+      </div>
+      {/* <div className="flex min-h-0 flex-col items-center my-10 gap-y-3">
         <div className="flex flex-wrap gap-1 max-w-[550px]">
           {techsIcon?.map((skill, id) => (
             <BlurFade key={skill.name} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
@@ -42,7 +57,7 @@ const Skills = () => {
             </BlurFade>
           ))}
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
