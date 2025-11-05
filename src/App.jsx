b7934985b73372714d68d28023b6c387b1fb3678
Sindor <162941627+sindorcoder@users.noreply.github.com>
2025-11-05 16:00:49 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Home from "./routes/home/Home.jsx";
+function App() {
+  return (
+    <div className="max-w-[800px] px-5 mx-auto">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
