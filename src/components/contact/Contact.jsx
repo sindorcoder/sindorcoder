7f9acbbf985d87344a601081d807a8c720f080b8
sindorcoder <sindordev77@gmail.com>
2025-11-24 16:07:16 +0500

    fixed bug  optimize code

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,7 +5,7 @@ import {Fragment} from "react";
 const BLUR_FADE_DELAY = 0.04;
 const Contact = () => {
     return (<>
-        <div className={"flex flex-wrap w-full justify-center gap-4"}>
+        <div className={"flex flex-wrap w-full justify-between gap-2"}>
             {social.map((social, index) => (<Fragment key={index}>
                 <BlurFade
                     delay={BLUR_FADE_DELAY * 10 + index * 0.1}
@@ -15,9 +15,9 @@ const Contact = () => {
                         href={social.url}
                         target={"_blank"}
                         rel="noopener noreferrer"
-                        className={"!bg-[#fff] rounded-lg hover:scale-110 duration-500 shadow border-[2px] border-[#f5f5f5] p-4"}
+                        className={"!bg-[#fff] rounded-lg hover:scale-110 duration-500 shadow border-[1px] border-[#f5f5f5] p-3.5"}
                     >
-                        {<social.icon size={20} strokeWidth={2}/>}
+                        {<social.icon size={24} strokeWidth={2}/>}
                     </a>
                 </BlurFade>
             </Fragment>))}
